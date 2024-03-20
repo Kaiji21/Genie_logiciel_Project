bbb91f6a4f098b3799de3fcd907c781fce34e9ff
Kaiji21 <elhaiba120@gmail.com>
2024-03-20 21:18:11 +0000

    Add QueueBorneeIntegerInterface and QueueBorneeInteger classes

--- src/main/java/Interfaces/QueueBorneeInteger.java
+++ src/main/java/Interfaces/QueueBorneeInteger.java
@@ -1,2 +1,52 @@
-package Interfaces;public class QueueBorneeInteger {
+package Interfaces;
+
+import java.util.ArrayList;
+
+public class QueueBorneeInteger implements QueueBorneeIntegerInterface {
+    private int capacite;
+    private ArrayList<Integer> queue;
+
+    public QueueBorneeInteger(int capacite) {
+        if (capacite < 0) {
+            throw new IllegalArgumentException("La capacité doit être supérieure ou égale à zéro");
+        }
+        this.capacite = capacite;
+        this.queue = new ArrayList<>();
+    }
+
+    @Override
+    public int capacite() {
+        return capacite;
+    }
+
+    @Override
+    public int taille() {
+        return queue.size();
+    }
+
+    @Override
+    public boolean estVide() {
+        return queue.isEmpty();
+    }
+
+    @Override
+    public boolean estPleine() {
+        return queue.size() == capacite;
+    }
+
+    @Override
+    public void ajouteElement(int element) throws IllegalStateException {
+        if (estPleine()) {
+            throw new IllegalStateException("La queue est pleine");
+        }
+        queue.add(element);
+    }
+
+    @Override
+    public int supprimeElement() throws IllegalStateException {
+        if (estVide()) {
+            throw new IllegalStateException("La queue est vide");
+        }
+        return queue.remove(0);
+    }
 }
--- src/main/java/Interfaces/QueueBorneeIntegerInterface.java
+++ src/main/java/Interfaces/QueueBorneeIntegerInterface.java
@@ -1,2 +1,10 @@
-package Interfaces;public interface QueueBorneeIntegerInterface {
+package Interfaces;
+
+public interface QueueBorneeIntegerInterface {
+    int capacite();
+    int taille();
+    boolean estVide();
+    boolean estPleine();
+    void ajouteElement(int element) throws IllegalStateException;
+    int supprimeElement() throws IllegalStateException;
 }
